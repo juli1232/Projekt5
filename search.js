99cf0467109ae2df30bf8b4f2274283cb7cd7a01
annemoerk <48016188+annemoerk@users.noreply.github.com>
2019-04-12 09:07:56 +0200

    Search bar

---
+++ search.js
@@ -0,0 +1,35 @@
+// Get input element
+    let filterInput = document.getElementById('filterInput');
+    // Add event listener
+    filterInput.addEventListener('keyup', filterItems);
+
+    function filterItems(){
+      // Get value of input
+      let filterValue = document.getElementById('filterInput').value.toLowerCase();
+
+      // Get items ul
+      let ul = document.getElementById('items');
+      // Get lis from ul
+      let li = ul.querySelectorAll('li.collection-item');
+
+      // Loop through collection-item lis
+      for(let i = 0;i < li.length;i++){
+        let a = li[i].getElementsByTagName('a')[0];
+        // If matched
+        if(a.innerHTML.toLowerCase().indexOf(filterValue) > -1){
+          li[i].style.display = '';
+        } else {
+          li[i].style.display = 'none';
+        }
+      }
+
+    }
+
+// Close and Open Search
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
