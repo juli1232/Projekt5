f44b3239e4784130623bdf1c4b630edbcb957620
annemoerk <48016188+annemoerk@users.noreply.github.com>
2019-04-12 09:33:07 +0200

    Slettet unødvendig js

--- search.js
+++ search.js
@@ -24,12 +24,3 @@
       }
 
     }
-
-// Close and Open Search
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
